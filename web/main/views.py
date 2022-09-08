7d5e97c423fa6403374c68e456b7dd133dda0a4c
wonjongKim <kwjdnjs@gmail.com>
2022-09-08 21:51:53 +0900

    add html files and bootstrap

--- web/main/views.py
+++ web/main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('CoFriend Home')
+    return render(request, 'main/home.html')
