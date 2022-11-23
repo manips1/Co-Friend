bb9d51bf300cf907a04cb1ae45e37b95de51b1c5
wonjongKim <kwjdnjs@gmail.com>
2022-11-23 13:44:49 +0900

    add editor

--- web/exercises/views.py
+++ web/exercises/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from exercises.api import api
 
 # Create your views here.
 
 
 def editor(request):
-    return render(request, 'exercises/editor.html')
+    problem_dict = api.create_problem(['print'])
+    context = {'problem': problem_dict['problem']}
+    return render(request, 'exercises/editor.html', context)
