31f5d34cfed1525d1e2303d4b4b58e97438ecc43
wonjongKim <kwjdnjs@gmail.com>
2022-12-10 16:27:25 +0900

    add iframe sharing

--- web/exercises/urls.py
+++ web/exercises/urls.py
@@ -6,4 +6,6 @@ app_name = 'exercises'
 urlpatterns = [
     path('', views.home, name='home'),
     path('editor/', views.editor, name='editor'),
+    path('share/', views.share, name='share'),
+    path('test/', views.test_page, name='test')
 ]
--- web/exercises/views.py
+++ web/exercises/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.clickjacking import xframe_options_exempt
 from exercises.api import api
 import json
 import base64
@@ -64,6 +65,50 @@ def editor(request):
     # get
     if request.method == 'GET':
         problem = request.GET.get('p', None)
+        b64_p = problem
+
+        # decode problem
+        problem = base64.b64decode(problem).decode('ascii')
+
+        # generate example code and result
+        ex_code = api.generate_code(problem)
+        ex_result = api.compile_code(ex_code, '1\r\n2\r\n3\r\n4')
+
+        context = {'problem': problem, 'ex_result':ex_result, 'ex_code':ex_code, 'base64_problem': b64_p}
+        return render(request, 'exercises/editor.html', context)
+
+
+@xframe_options_exempt
+def share(request):
+    """exercises editor share page view
+
+    Args:
+        request (request): django request
+        problem_id (string): base64 encoded problem
+
+    """
+    # post
+    if request.method == 'POST':
+        # data from user
+        data = json.loads(request.body)
+
+        # compile the code
+        compile_output = api.compile_code(data['source_code'], data['stdin'])
+
+        # response
+        response_data = {
+            'status':'ok',
+            'stdout':'',
+            'compile_output': compile_output,
+            'time':'0',
+            'memory':'0'
+        }
+
+        return JsonResponse(response_data)
+
+    # get
+    if request.method == 'GET':
+        problem = request.GET.get('p', None)
 
         # decode problem
         problem = base64.b64decode(problem).decode('ascii')
@@ -73,4 +118,8 @@ def editor(request):
         ex_result = api.compile_code(ex_code, '1\r\n2\r\n3\r\n4')
 
         context = {'problem': problem, 'ex_result':ex_result, 'ex_code':ex_code}
-        return render(request, 'exercises/editor.html', context)
+        return render(request, 'exercises/share_editor.html', context)
+
+
+def test_page(request):
+    return render(request, 'exercises/test_page.html')
