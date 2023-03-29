526073116011c591fb02d55d8f0fd7fe8e37d41f
manips <jwjw6410@gmail.com>
2023-03-29 15:30:15 +0900

    로그인 버튼 추가 진행중

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from allauth.account.views import confirm_email
 
 urlpatterns = [
     path('', views.home),
     path('login/', views.login, name = 'login'),
-    
+    path('accounts/', include('allauth.urls')),
 ]
