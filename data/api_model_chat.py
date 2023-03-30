90ab7cd39e7592e3d1fe0b80d2bbe11d5c41cd8b
wonjongKim <kwjdnjs@gmail.com>
2023-03-30 13:07:23 +0900

    add grade code

--- data/api_model_chat.py
+++ data/api_model_chat.py
@@ -6,34 +6,38 @@ import json
 """
 request_str = 'Create a python problem sentence about print and if.'
 
-request_str2 = """
-Code A is:
+code_a = """
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
 print(last_name + " " + first_name)
+"""
 
-Code B is:
+code_b = """
 fname = input("Input your First Name : ")
 lname = input("Input your Last Name : ")
 print ("Hello  " + lname + " " + fname)
+"""
+
+request_str2 = """
+Code A is:
+{}
+
+Code B is:
+{}
 
 Code B is a answer code.
 
 Grade Code A.
 
-Please answer this format:
-['grade': True or False,
-'reason':text]
-"""
+Please answer this Json format:
+'pass': True or False,
+'score': 0~100,
+'reason':text
+""".format(code_a, code_b)
 
 """
 API request & response
-
-model: 모델 이름
-prompt: 요청 내용
-temperature: 값이 낮을수록 안정적인 값 출력
-stop: 문장 출력 종료 조건
 """
 response = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
@@ -45,5 +49,9 @@ response = openai.ChatCompletion.create(
 json_object = json.loads(response.__str__())
 result_text = json_object['choices'][0]['message']['content']
 
+json_object = json.loads(result_text)
+
 #결과 출력
-print(result_text + '.')
+print(json_object['pass'])
+print(json_object['score'])
+print(json_object['reason'])
