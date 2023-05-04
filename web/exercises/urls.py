0ee2fc8277643397707472c03705b2163f05fd45
wonjongKim <kwjdnjs@gmail.com>
2023-05-04 13:20:01 +0900

    add page

--- web/exercises/urls.py
+++ web/exercises/urls.py
@@ -5,6 +5,7 @@ app_name = 'exercises'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('problem_type/', views.problem_type, name='problem_type'),
     path('editor/', views.editor, name='editor'),
     path('share/', views.share, name='share'),
     path('grade/', views.grade, name='grade'),
